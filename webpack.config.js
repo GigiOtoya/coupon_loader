f17c60b1b18943a5e318b3e32cc4bb8c32d7e91a
Luigi Otoya <Luigi.Otoya@gmail.com>
2025-04-04 01:41:35 -0400

    config fix

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+/**
+ * @type {import('webpack').Configuration}
+ */
+module.exports = {
+  entry: {
+    "background/background": "./src/background/background.ts",
+    "popup/popup": "./src/popup/popup.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "src/manifest.json" },
+        { from: "src/popup/popup.html", to: "popup" },
+        { from: "src/styles", to: "styles" },
+        { from: "src/images", to: "images" },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  mode: "production",
+};
