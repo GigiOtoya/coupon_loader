bf834e552d18a4417a9dd0085025ed712d81f255
Luigi Otoya <Luigi.Otoya@gmail.com>
2025-04-12 20:26:03 -0400

    matching iframe

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,11 +1,37 @@
-// const unloaded = document.querySelector("a.login-to-load");
-chrome.runtime.onMessage.addListener((message: string) => {
-  if (message === "action") {
-    console.log("action testing...");
-    const doc = document.querySelector("image.v39pjb");
-    console.log(doc);
-    return true;
+const storeMethods: { [k: string]: Function } = {
+  shoprite: shoprite,
+  stopandshop: stopandshop,
+};
+
+function stopandshop() {
+  console.log("stopandshop");
+}
+
+function shoprite() {
+  const doc = document.querySelectorAll("a.login-to-load");
+  console.log(doc);
+  return;
+}
+
+function loadCoupons(store: string) {
+  if (storeMethods[store]) {
+    storeMethods[store]();
+  }
+  return;
+}
+
+chrome.runtime.onMessage.addListener((message: { action: string; domain: string }) => {
+  if (message.action === "load") {
+    console.log(`Store: ${message.domain}`);
+    loadCoupons(message.domain);
   }
 
-  return false;
+  // if (message.action === "load") {
+  //   console.log("action testing...");
+  //   loadShoprite();
+  // } else {
+  //   console.log("false");
+  // }
+
+  return undefined;
 });
--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,19 +1,49 @@
-const actionBtn: HTMLButtonElement | null = document.querySelector("#action-btn");
+import { parse } from "tldts";
 
+const urls: { [key: string]: string } = {
+  "https://www.shoprite.com/sm/pickup/rsid/3000/digital-coupon?cfrom=homenavigation": "shoprite",
+  "https://www.shoprite.com/sm/pickup/rsid/3000/digital-coupon?cfrom=menuicon": "shoprite",
+  "https://stopandshop.com/savings/coupons/browse": "stopandshop",
+};
+
+const targetFrames: { [key: string]: string } = {
+  "stopandshop.com": "stopandshop",
+  "shop-rite-web-prod.azurewebsites.net": "shoprite",
+};
+
+const actionBtn: HTMLButtonElement | null = document.querySelector("#action-btn");
 actionBtn?.addEventListener("click", async () => {
-  const url = "https://www.shoprite.com/sm/pickup/rsid/3000/digital-coupon?cfrom=menuicon";
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  if (!tab?.id) {
+  if (!tab?.id || !tab.url) {
     return;
   }
-  console.log(tab);
 
-  if (tab.url === url) {
+  if (tab.url && tab.url in urls) {
+    const frames = await chrome.webNavigation.getAllFrames({ tabId: tab.id });
+    if (!frames) {
+      return;
+    }
+
+    let matchingFrame = { id: 0, hostName: "" };
+    for (const frame of frames) {
+      const hostName = new URL(frame.url).hostname;
+      if (hostName in targetFrames) {
+        matchingFrame = { id: frame.frameId, hostName };
+        break;
+      }
+    }
+
+    console.log(matchingFrame);
+    if (!matchingFrame.hostName) {
+      return;
+    }
     chrome.scripting.executeScript({
-      target: { tabId: tab.id },
+      target: { tabId: tab.id, frameIds: [matchingFrame.id] },
       files: ["/content/content.js"],
     });
+    chrome.tabs.sendMessage(tab.id, {
+      action: "load",
+      domain: targetFrames[matchingFrame.hostName],
+    });
   }
-
-  chrome.tabs.sendMessage(tab.id, "action");
 });
