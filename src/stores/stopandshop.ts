757b924ce5a8eb517e200352c3abc3ac65032123
Luigi Otoya <Luigi.Otoya@gmail.com>
2025-04-16 22:15:52 -0400

    sns

--- src/stores/stopandshop.ts
+++ src/stores/stopandshop.ts
@@ -1,5 +1,41 @@
 const stopandshop = async () => {
-  console.log("connected");
+  const selectors = {
+    container: "ul.tile-list",
+    loadMore: "#show-more",
+  };
+
+  const loadItems = async (count: number): Promise<number> => {
+    return new Promise((resolve) => {
+      const container = document.querySelector<HTMLUListElement>(selectors.container);
+
+      if (!container) {
+        return resolve(count);
+      }
+
+      const mutationObserver = new MutationObserver(() => {
+        const newCount = container.childElementCount;
+        if (newCount > count) {
+          mutationObserver.disconnect();
+          resolve(newCount);
+        }
+      });
+      mutationObserver.observe(container, { childList: true });
+    });
+  };
+  const container = document.querySelector<HTMLUListElement>(selectors.container);
+  if (!container) {
+    return;
+  }
+
+  let itemCount = container.childElementCount;
+  let showMore = document.querySelector<HTMLButtonElement>(selectors.loadMore);
+
+  while (showMore) {
+    showMore.click();
+    itemCount = await loadItems(itemCount);
+    showMore = document.querySelector<HTMLButtonElement>(selectors.loadMore);
+    console.log("loading...");
+  }
   return;
 };
 
